c91296340a6ffbcc4b7b323be36fbfd8803b3ace
MuthazhahiHMRC <89528156+MuthazhahiHMRC@users.noreply.github.com>
2021-11-09 16:39:13 +0000

    it-tests_Configured the setup for it_tests

--- it/connectors/NctsConnectorISpec.scala
+++ it/connectors/NctsConnectorISpec.scala
@@ -1,7 +1,31 @@
 package connectors
 
+import com.github.tomakehurst.wiremock.client.WireMock._
+import org.scalatest.concurrent.ScalaFutures
+import org.scalatestplus.play.PlaySpec
+import play.api.http.Status
+import uk.gov.hmrc.http.HeaderCarrier
 import utils.SpecCommonHelper
 
-class NctsConnectorISpec extends SpecCommonHelper {
+class NctsConnectorISpec extends PlaySpec with SpecCommonHelper with ScalaFutures{
+
+  val connector = app.injector.instanceOf[NctsConnector]
+  implicit val hc=HeaderCarrier()
+  "check status" should {
+    "return the service status of the Web channel" in{
+
+      wireMock.wireMockServer.stubFor(
+        get(urlMatching("/service-status"))
+          .willReturn(
+            aResponse()
+              .withStatus(Status.OK)
+              .withBody("")
+          )
+      )
+      val serviceStatus=connector.checkStatus()
+
+    }
+
+  }
 
 }
