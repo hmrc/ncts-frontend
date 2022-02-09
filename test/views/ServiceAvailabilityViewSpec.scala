ce1d1594dd91dfc6177a96b6abc751a77144a480
gorkemunsal <37540294+gorkemunsal@users.noreply.github.com>
2022-02-09 17:24:12 +0000

    NSP-294 | added refresh this page link (#32)
    * NSP-294 changed last updated text
    
    * NSP-0294 added refresh page link for submission channels
    
    * NSP-294 updated tests
    
    * NSP-294 refactor text alignment

--- test/views/ServiceAvailabilityViewSpec.scala
+++ test/views/ServiceAvailabilityViewSpec.scala
@@ -23,6 +23,7 @@ import org.jsoup.nodes.Document
 import play.api.test.Injecting
 import utils.DateTimeFormatter
 import utils.HealthDetailsExamples._
+import utils.DateTimeFormatter.formatDateTime
 import views.html.ServiceAvailability
 
 import java.time.LocalDateTime
@@ -117,6 +118,34 @@ class ServiceAvailabilityViewSpec extends SpecBase with Injecting {
 
       document.getElementsByClass("govuk-table__cell").get(10)
         .text() mustBe messages("service.availability.submission.channels.status.xml.channel")
+    }
+
+    "should show the last updated time with refresh link for arrivals" in {
+      val lastUpdatedText = s"${messages("service.availability.lastUpdated")} " +
+        s"${formatDateTime(LocalDateTime.of(2022, 1, 24, 0, 0, 0).minusMinutes(30))}. " +
+        s"${messages("service.availability.lastUpdated.refresh")} " +
+        s"${messages("service.availability.lastUpdated.latest")}"
+
+      document.getElementsByClass("govuk-inset-text").first()
+        .text() mustBe lastUpdatedText
+    }
+    "should show the last updated time with refresh link for departures" in {
+      val lastUpdatedText = s"${messages("service.availability.lastUpdated")} " +
+        s"${formatDateTime(LocalDateTime.of(2022, 1, 24, 0, 0, 0).minusSeconds(30))}. " +
+        s"${messages("service.availability.lastUpdated.refresh")} " +
+        s"${messages("service.availability.lastUpdated.latest")}"
+
+      document.getElementsByClass("govuk-inset-text").get(1)
+        .text() mustBe lastUpdatedText
+    }
+    "should show the last updated time with refresh link for other systems" in {
+      val lastUpdatedText = s"${messages("service.availability.lastUpdated")} " +
+        s"${formatDateTime(LocalDateTime.of(2022, 1, 24, 0, 0, 0))}. " +
+        s"${messages("service.availability.lastUpdated.refresh")} " +
+        s"${messages("service.availability.lastUpdated.latest")}"
+
+      document.getElementsByClass("govuk-inset-text").get(2)
+        .text() mustBe lastUpdatedText
     }
 
     "should have a get help link" in {
