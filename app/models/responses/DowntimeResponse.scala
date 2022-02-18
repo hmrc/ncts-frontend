0196331830a2938eddc940000e1025af26bc0ea7
gorkemunsal <gorkem.unsal@accenture.com>
2022-02-18 17:01:23 +0000

    NSP-331 wip

--- app/models/responses/DowntimeResponse.scala
+++ app/models/responses/DowntimeResponse.scala
@@ -1,7 +1,22 @@
+/*
+ * Copyright 2022 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package models.responses
 
 import models.Channel
-import models.Channel.Channel
 import models.responses.ErrorResponse.DowntimeResponseError
 import org.slf4j.LoggerFactory
 import play.api.http.Status.OK
@@ -9,6 +24,12 @@ import play.api.libs.json.{JsError, JsSuccess, Json, OFormat}
 import uk.gov.hmrc.http.{HttpReads, HttpResponse}
 
 import java.time.LocalDateTime
+
+case class Downtime(affectedChannel: Channel.Value, start: LocalDateTime, end: LocalDateTime)
+
+object Downtime {
+  implicit val format = Json.format[Downtime]
+}
 
 case class DowntimeResponse(downtimes: Seq[Downtime])
 
@@ -36,8 +57,3 @@ object DowntimeResponse {
   }
 }
 
-case class Downtime(affectedChannel: Channel.Value, start: LocalDateTime, end: LocalDateTime)
-
-object Downtime {
-  implicit val format = Json.format[Downtime]
-}
