9140a7be928adcd410db590efdc0b994b1b10456
dnyuar <60218795+dnyuar@users.noreply.github.com>
2024-04-05 11:02:53 +0100

    Planned outage remove content

--- test/views/PlannedDowntimeViewSpec.scala
+++ test/views/PlannedDowntimeViewSpec.scala
@@ -74,22 +74,167 @@ class PlannedDowntimeViewSpec extends SpecBase with Injecting {
         .attr("href") mustBe getHelpUrl
     }
 
-    "when there is downtime planned" - {
+    "when there is downtime planned for arrivals" - {
 
-      "should have the correct date message" in {
-        documentNoDowntime.select("#main-content > p:nth-child(2)").text() mustBe messages("planned-downtime.temp.p1")
+      "should have the arrivals subheading" in {
+        document.select("h2").first().text() mustBe messages("planned-downtime.ncts.arrivals")
       }
 
-      "should have the correct subheading" in {
-        documentNoDowntime.select("#main-content > h2").text() mustBe messages("planned-downtime.temp.h2")
+      "should have a table for arrivals with the correct heading" in {
+        document.select("table:nth-child(2) > thead > tr > th:nth-child(1)").get(0).text() mustBe messages(
+          "planned-downtime.system.core.name"
+        )
+        document.select("table:nth-child(2) > thead > tr > th:nth-child(2)").get(0).text() mustBe messages(
+          "planned-downtime.start"
+        )
+        document.select("table:nth-child(2) > thead > tr > th:nth-child(3)").get(0).text() mustBe messages(
+          "planned-downtime.end"
+        )
       }
 
-      "should have the correct first paragraph" in {
-        documentNoDowntime.select("#main-content > p:nth-child(4)").text() mustBe messages("planned-downtime.temp.p2")
+      "should have a table for arrivals with a row for GB" in {
+        document
+          .select("table:nth-child(2) > tbody > tr:nth-child(1) > td:nth-child(1)")
+          .get(0)
+          .text() mustBe messages("planned-downtime.ncts.gb.arrivals")
+
+        document
+          .select("table:nth-child(2) > tbody > tr:nth-child(1) > td:nth-child(2)")
+          .get(0)
+          .text() mustBe "Saturday 1 January 2022 8am GMT"
+
+        document
+          .select("table:nth-child(2) > tbody > tr:nth-child(1) > td:nth-child(3)")
+          .get(0)
+          .text() mustBe "Wednesday 1 June 2022 8pm BST"
       }
 
-      "should have the correct second paragraph" in {
-        documentNoDowntime.select("#main-content > p:nth-child(5)").text() mustBe messages("planned-downtime.temp.p3")
+      "should have a table for arrivals with a row for XI" in {
+        document
+          .select("table:nth-child(2) > tbody > tr:nth-child(2) > td:nth-child(1)")
+          .get(0)
+          .text() mustBe messages("planned-downtime.ncts.xi.arrivals")
+
+        document
+          .select("table:nth-child(2) > tbody > tr:nth-child(2) > td:nth-child(2)")
+          .get(0)
+          .text() mustBe "Saturday 1 January 2022 8am GMT"
+
+        document
+          .select("table:nth-child(2) > tbody > tr:nth-child(2) > td:nth-child(3)")
+          .get(0)
+          .text() mustBe "Wednesday 1 June 2022 8pm BST"
+      }
+
+      "should have some content about submissions when the service is down and an apology for arrivals" in {
+        document.getElementsByClass("govuk-body").first().text() mustBe messages("planned-downtime.p1")
+        document.getElementsByClass("govuk-body").get(1).text() mustBe messages("planned-downtime.p2")
+      }
+
+      "should have a related links section" - {
+
+        "with a link to service availability" in {
+          document.getElementsByTag("h2").get(2).text() mustBe messages("service.availability.related.links")
+          val link = document.select("#main-content > div:nth-child(3) > div > ul > li:nth-child(1) > a")
+
+          link.text() mustBe messages("planned-downtime.related.links.service.availability")
+          link.attr("href") mustBe "/new-computerised-transit-system-service-availability" +
+            "/service-availability"
+        }
+
+        "with a link to downtime history" in {
+          val link = document.select("#main-content > div:nth-child(3) > div > ul > li:nth-child(2) > a")
+
+          link.text() mustBe messages("planned-downtime.related.links.downtime.history")
+          link.attr("href") mustBe "/new-computerised-transit-system-service-availability" +
+            "/downtime-history"
+        }
+      }
+    }
+
+    "when there is downtime planned for departures" - {
+
+      "should have the departures subheading" in {
+        document.select("h2").get(1).text() mustBe messages("planned-downtime.ncts.departures")
+      }
+
+      "should have a table for departures with the correct heading" in {
+        val table = document.select("table:nth-child(5) > thead > tr")
+        table.select("th:nth-child(1)").get(0).text() mustBe messages("planned-downtime.system.core.name")
+        table.select("th:nth-child(2)").get(0).text() mustBe messages("planned-downtime.start")
+        table.select("th:nth-child(3)").get(0).text() mustBe messages("planned-downtime.end")
+      }
+
+      "should have a table for departures with a row for GB" in {
+
+        val tableRow = document.select("table:nth-child(5) > tbody > tr:nth-child(1)")
+
+        tableRow
+          .select("td:nth-child(1)")
+          .get(0)
+          .text() mustBe messages("planned-downtime.ncts.gb.departures")
+
+        tableRow
+          .select("td:nth-child(2)")
+          .get(0)
+          .text() mustBe "Saturday 1 January 2022 8am GMT"
+
+        tableRow
+          .select("td:nth-child(3)")
+          .get(0)
+          .text() mustBe "Wednesday 1 June 2022 8pm BST"
+      }
+
+      "should have a table for departures with a row for XI" in {
+
+        val tableRow = document.select("table:nth-child(5) > tbody > tr:nth-child(2)")
+
+        tableRow
+          .select("td:nth-child(1)")
+          .get(0)
+          .text() mustBe messages("planned-downtime.ncts.xi.departures")
+
+        tableRow
+          .select("td:nth-child(2)")
+          .get(0)
+          .text() mustBe "Saturday 1 January 2022 8am GMT"
+
+        tableRow
+          .select("td:nth-child(3)")
+          .get(0)
+          .text() mustBe "Wednesday 1 June 2022 8pm BST"
+      }
+
+      "should have some content about submissions when the service is down and an apology for departures" in {
+        document.getElementsByClass("govuk-body").get(0).text() mustBe messages("planned-downtime.p1")
+        document.getElementsByClass("govuk-body").get(1).text() mustBe messages("planned-downtime.p2")
+      }
+    }
+
+    "when there is no downtime planned" - {
+
+      "should not have any tables of downtime information" in {
+        documentNoDowntime.getElementsByTag("table").size() mustBe 0
+      }
+
+      "should have the arrivals subheading" in {
+        documentNoDowntime.select("h2").first().text() mustBe messages("planned-downtime.ncts.arrivals")
+      }
+
+      "should have text about there being no downtime for arrivals" in {
+        documentNoDowntime.getElementsByClass("govuk-body").first().text() mustBe messages(
+          "planned-downtime.no.downtime.planned.arrivals"
+        )
+      }
+
+      "should have the departures subheading" in {
+        documentNoDowntime.select("h2").get(1).text() mustBe messages("planned-downtime.ncts.departures")
+      }
+
+      "should have text about there being no downtime for departures" in {
+        documentNoDowntime.getElementsByClass("govuk-body").get(1).text() mustBe messages(
+          "planned-downtime.no.downtime.planned.departures"
+        )
       }
     }
   }
