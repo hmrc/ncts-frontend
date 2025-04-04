b2aecbebb377b6d022d4e2418ea2bccb8ec22bdd
Liam-Macarthur <113617571+Liam-Macarthur@users.noreply.github.com>
2025-04-04 16:34:46 +0100

    DDCYLS-6382 - Updating README (#126)
    * Updating README content
    
    * correcting links
    
    * updating sbt and scala versions
    
    * Updating scoverage and fixing white space format
    
    * Updating scoverage
    
    * updating hmrc play version

--- build.sbt
+++ build.sbt
@@ -22,7 +22,7 @@ import uk.gov.hmrc.versioning.SbtGitVersioning.autoImport.majorVersion
 
 lazy val appName: String = "ncts-frontend"
 
-ThisBuild / scalaVersion := "2.13.12"
+ThisBuild / scalaVersion := "2.13.16"
 ThisBuild / majorVersion := 0
 
 lazy val root = (project in file("."))
--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -22,8 +22,8 @@ object AppDependencies {
 
   val compile: Seq[ModuleID] = Seq(
     play.sbt.PlayImport.ws,
-    "uk.gov.hmrc" %% "play-frontend-hmrc-play-30"         % "11.12.0",
-    "uk.gov.hmrc" %% "bootstrap-frontend-play-30" % bootstrapVersion
+    "uk.gov.hmrc" %% "play-frontend-hmrc-play-30"         % "11.13.0",
+    "uk.gov.hmrc" %% "bootstrap-frontend-play-30"         % bootstrapVersion
   )
 
   val test: Seq[ModuleID] = Seq(
