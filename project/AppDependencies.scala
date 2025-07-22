0d1c344f4dcc6ba14318c62fe6bfd364f596b892
Bon Parker <45689313+bonparker@users.noreply.github.com>
2025-07-22 13:54:53 +0100

    DDCYLS-6995: Bootstrap 19.8.0 upgrade (#129)
    * DDCYLS-6995: Bootstrap 19.8.0 upgrade
    
    * DDCYLS-6995: App Dependency Updates

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -18,11 +18,11 @@ import sbt.*
 
 object AppDependencies {
 
-  val bootstrapVersion = "9.13.0"
+  val bootstrapVersion = "9.18.0"
 
   val compile: Seq[ModuleID] = Seq(
     play.sbt.PlayImport.ws,
-    "uk.gov.hmrc" %% "play-frontend-hmrc-play-30"         % "12.6.0",
+    "uk.gov.hmrc" %% "play-frontend-hmrc-play-30"         % "12.7.0",
     "uk.gov.hmrc" %% "bootstrap-frontend-play-30"         % bootstrapVersion
   )
 
